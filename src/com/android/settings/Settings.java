7b00d7c2f859bd375bf9c8a94142be6b7ae638f0
maxwen <max.weninger@gmail.com>
2014-08-08 16:49:59 +0700

    Add ArtStarOS dashboard entry

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,4 +178,7 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // ArtStar Settings
+    public static class ArtstarSettingsActivity extends SettingsActivity {}
+
 }
